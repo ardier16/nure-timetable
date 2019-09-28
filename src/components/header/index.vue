adda84ff3c15bb893cd6a8a75e07e306722588fd
ardier16 <ardier16@gmail.com>
2019-09-28 11:40:44 +0300

    Add locales

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="header">
-    Header
+    {{ 'header.title' | translate }}
   </header>
 </template>
 
---
+++ src/constants/locales.js
@@ -0,0 +1,18 @@
+import { config } from '@/config'
+
+export const locales = {
+  en: {
+    text: 'ENG',
+    isoCode: 'en',
+  },
+  ru: {
+    text: 'RUS',
+    isoCode: 'ru',
+  },
+  uk: {
+    text: 'UKR',
+    isoCode: 'uk',
+  },
+}
+
+export const DEFAULT_LOCALE = config.LOCALE
