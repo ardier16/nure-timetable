eb382b209036e81363f4bc9d197221756b720157
ardier16 <ardier16@gmail.com>
2019-09-28 15:07:56 +0300

    Add API service

--- src/main.js
+++ src/main.js
@@ -5,17 +5,21 @@ import VueHead from 'vue-head'
 
 import './devtools'
 import { initI18n } from './i18n'
+import { initApi } from './api'
 
 import translate from '@utils/translate'
 
 import App from './components/app'
 
 import store from '@store'
+import { config } from '@/config'
 
 import '../node_modules/@mdi/font/css/materialdesignicons.min.css'
 
 async function init () {
   initI18n(store)
+  initApi(config.API_URL)
+
   Vue.config.productionTip = false
 
   Vue.use(VueScrollTo, { offset: -25 })
