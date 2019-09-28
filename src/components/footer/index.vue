79f9614e718f7ad9ba972e50f870c60e3770f1f7
ardier16 <ardier16@gmail.com>
2019-09-28 13:02:57 +0300

    Use helvetica neue font

--- src/components/footer/index.vue
+++ src/components/footer/index.vue
@@ -1,12 +1,26 @@
 <template>
   <footer class="footer">
-    Footer
+    <img
+      class="footer__logo"
+      src="@assets/img/logo-full.svg"
+    >
+
+    <p class="footer__copyright">
+      Created by @ardier16
+    </p>
+
+    <social-networks />
   </footer>
 </template>
 
 <script>
+import SocialNetworks from './components/social-networks'
+
 export default {
   name: 'app-footer',
+  components: {
+    SocialNetworks,
+  },
 }
 </script>
 
@@ -18,5 +32,19 @@ export default {
   color: $color-text-inverse;
   padding-top: 1.2rem;
   padding-bottom: 1.2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  &__logo {
+    max-height: 2.8rem;
+  }
+
+  &__copyright {
+    font-weight: 100;
+    color: $color-secondary;
+    font-size: 1.6rem;
+    letter-spacing: 0.1rem;
+  }
 }
 </style>
